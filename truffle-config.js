19ef3737d674c731b0382c8f7056d200574ec169
edwardca12@gmail.com <edwardca12@gmail.com>
2019-12-24 15:31:47 -0500

    deployed

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// 12-word mnemonic
+var mnemonic = process.env.NMEMONIC || "deliver private phone screen snake surge flight anxiety cousin prosper spawn shrug"
+
 module.exports = {
   networks: {
-    ropsten: {
+    rinkeby: {
       provider: function() {
         return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/989cb18083114110ba7f613d29397642");
       },
@@ -8,7 +13,7 @@ module.exports = {
     },
     test: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/");
       },
       network_id: '*',
     },
